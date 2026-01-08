2ea0637c9ea753de45704400c4e966f3ef639c81
UMAL WARA <umalwaraarain123786@gmail.com>
2026-01-08 16:55:03 +0500

    Add main method to Arrays class for matrix input

--- Matrix Spiral.java
+++ Matrix Spiral.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 public class Arrays {
+public static void main(String[] args) {
 //Spiral order matrix
 Scanner sc = new Scanner(System.in);
 System.out.println("Enter order of matrix (m*n)");
